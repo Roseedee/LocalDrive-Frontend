58b6be7530ced45ab7f146e949ef81f27cbec858
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2026-04-03 01:28:20 +0700

    update

--- src/modules/auth/components/ProtectedLayout.tsx
+++ src/modules/auth/components/ProtectedLayout.tsx
@@ -1,20 +1,29 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { useAuthStore } from "../store/auth.store"
+import { logout } from "@/shared/lib/axios"
+
+import "@/shared/styles/loading.css"
 
 export default function ProtectedLayout() {
   const user_id = useAuthStore(s => s.user_id)
   const loading = useAuthStore(s => s.loading)
   const isSessionExpired = useAuthStore(s => s.isSessionExpired)
 
-  if (loading) return <div>Loading...</div>
-  
-  if (isSessionExpired) {
-    return <Navigate to="/init" replace />
+  if (!loading && (isSessionExpired || !user_id)) {
+    logout();
+    // return <Navigate to="/init" replace />
   }
 
-  if (!user_id) {
-    return <Navigate to="/init" replace />
-  }
+  return (
+    <>
+      <Outlet />
 
-  return <Outlet />
+      {loading && (
+        <div className="loading-overlay">
+          <div className="spinner"></div>
+          <span className="loading-text">กำลังโหลด</span>
+        </div>
+      )}
+    </>
+  )
 }
--- src/modules/files/pages/FilesPage.tsx
+++ src/modules/files/pages/FilesPage.tsx
@@ -3,6 +3,10 @@ import { useAuthStore } from "@/modules/auth/store/auth.store";
 export default function FilesPage() {
   const { user_id, device_name } = useAuthStore();
 
+  const handleSetExpired = () => {
+    useAuthStore.getState().expireSession();
+  }
+
   return (
     <div className="content">
       <h1>Files</h1>
@@ -10,6 +14,17 @@ export default function FilesPage() {
       <p>User ID: {user_id}</p>
       <p>Device Name: {device_name}</p>
       <a href="/logout">Logout</a>
+      <div>
+        {/* <div className="loading-overlay">
+          <div className="spinner"></div>
+          <span className="loading-text">กำลังโหลด</span>
+        </div> */}
+      </div>
+
+      <button onClick={handleSetExpired} style={{ marginTop: "20px", padding: "10px 20px", backgroundColor: "#f44336", color: "#fff", border: "none", borderRadius: "4px", cursor: "pointer" }}>
+        set expired
+      </button>
     </div>
+
   );
 }
