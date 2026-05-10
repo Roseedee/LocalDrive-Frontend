79eab0c72d4378f6699e0e5519402b6150cdd156
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2026-05-11 01:00:36 +0700

    feat: register page

--- src/modules/auth/pages/InitPage.tsx
+++ src/modules/auth/pages/InitPage.tsx
@@ -3,9 +3,14 @@ import { useNavigate } from "react-router-dom"
 import { register } from "../api/auth.api"
 import { useAuthStore } from "../store/auth.store"
 
+import '../styles/initPage.css'
+import '../styles/global.css'
+
 export default function RegisterPage() {
     const navigate = useNavigate()
     const [deviceName, setDeviceName] = useState("")
+    const [password, setPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
     const setSession = useAuthStore(s => s.setSession)
 
     const handleSubmit = async () => {
@@ -28,16 +33,26 @@ export default function RegisterPage() {
     }
 
     return (
-        <div>
-            <a href="/">Back to Home</a>
-            Register Page
-            <form action="" onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit();
-            }}>
-                <input type="text" placeholder="Device Name" value={deviceName} onChange={(e) => setDeviceName(e.target.value)} />
-                <button type="submit">Register</button>
-            </form>
+        <div className="auth-container">
+            <div className="auth-card">
+                <h2>สร้างบัญชีผู้ใช้ใหม่</h2>
+                <form action="" onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
+                }}>
+                    <input type="text" placeholder="ชื่อผู้ใช้" value={deviceName} onChange={(e) => setDeviceName(e.target.value)} />
+                    <input type={showPassword ? "text" : "password"} placeholder="รหัสผ่าน" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <label htmlFor="showPassword">
+                        <input type="checkbox" id="showPassword" checked={showPassword} onChange={(e) => setShowPassword(e.target.checked)} />
+                        แสดงรหัสผ่านเป็นข้อความ
+                    </label>
+                    <button type="submit">สร้างบัญชี</button>
+                    <a href="/login" className="link">
+                        มีบัญชีอยู่แล้ว? ลงชื่อเข้าใช้
+                    </a>
+                </form>
+
+            </div>
         </div>
     )
 }
